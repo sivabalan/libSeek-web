184359f655d84aae9e4c2f8e886e435984caa601
Sivabalan Thirunavukkarasu <siva886@gmail.com>
2014-05-30 19:18:31 -0700

    Removed autosuggest, added search placeholder

--- FinalSet/Webpage/js/logic.js
+++ FinalSet/Webpage/js/logic.js
@@ -3,45 +3,13 @@ var gVar;
 function init() {
 
 	var searchBox = document.getElementById('searchText'),
-		$sBoxObj = $(searchBox),
-		$asObj = $('#autoSuggestDiv');
+		$sBoxObj = $(searchBox);
 
 	searchBox.addEventListener('input',function(e){
-		
-		var searchQuery = e.target.value,
-			asQueryObj = getWordAtPosition(searchQuery,e.target.selectionStart);
 
-		$asObj.hide();
 		resetResults('Your software libraries search results will appear here!');
-
-		if (e.target.value.slice(-1) != ' ' && $.trim(e.target.value) != '' && $.trim($asObj.text()) != asQueryObj.word)
-		{	
-			if(asQueryObj.partial_word.length >= 3) 
-			{
-				autoSuggest(e.target,asQueryObj);
-			}
-		}
+		
 	});
-
-	$asObj.css('top',($sBoxObj.offset().top + $sBoxObj.outerHeight()).toString()+'px');
-
-	function acceptAutoSuggest() {
-		var currentQuery = $sBoxObj.val(),
-			cursorPos = searchBox.selectionStart,
-			curwordObj = getWordAtPosition(currentQuery,cursorPos),
-			asText = $.trim($asObj.text());
-
-		$sBoxObj.val(currentQuery.replace(curwordObj.word,asText));
-
-		searchBox.selectionStart = curwordObj.end + asText.length - curwordObj.word.length;
-		searchBox.selectionEnd = curwordObj.end + asText.length - curwordObj.word.length;	
-
-		setTimeout(function(){
-	        $sBoxObj.focus();
-	    }, 1);
-
-		$asObj.hide();
-	}
 
 	$(document).keydown(function(e) {
 
@@ -63,15 +31,6 @@ function init() {
 	        	sendSearchRequest();
 	        	return;
 	    	}
-	    	else if(keynum == 9 || keynum == 39)
-	    	{
-	    		if($asObj.css('display') != 'none')
-				{
-	    			acceptAutoSuggest();
-	    			if(keynum == 9)
-	    				e.preventDefault();
-	    		}
-	    	}    
         }
 	    
 	});
@@ -84,75 +43,6 @@ function init() {
         $sBoxObj.focus();
     }, 1);
 
-    $asObj.click(function(){
-		acceptAutoSuggest();
-	});
-
-}
-
-function getWordAtPosition(inputString, cPosition) {
-	var i = cPosition,
-		j = cPosition,
-		wordObj = {};
-
-	while(i > 0 && inputString[i-1] != ' ') {
-		i--;
-	}
-
-	while(j < inputString.length && inputString[j] != ' ') {
-		j++;
-	}
-
-	wordObj['word'] = inputString.slice(i,j);
-	wordObj['start'] = i;
-	wordObj['end'] = j;
-	wordObj['partial_word'] = inputString.slice(i,cPosition);
-
-	return wordObj;
-}
-
-function autoSuggest(inputObj,asWordObj) {
-	var $sBoxObj = $(inputObj),
-		$asObj = $('#autoSuggestDiv'),
-		asFixedLeft = $sBoxObj.offset().left + parseInt($sBoxObj.css('padding-left').replace('px','')),
-		asFixedOffset = parseInt($sBoxObj.css('padding-left').replace('px','')),
-		asOffset = $('#asSizeCheck')
-						.css('display','none')
-						.css('color','#fff')
-						.css('position','absolute')
-						.css('font-size',$sBoxObj.css('font-size'))
-						.css('width','auto')
-						.html($sBoxObj.val().slice(0,asWordObj.start))
-						.width();
-
-		asOffset = asWordObj.start > 0 ? asOffset : asOffset - 2; 
-
-		$asObj.css('left',(asFixedLeft+asOffset+1).toString()+'px');
-	$.ajax({
-		type: "GET",
-		url: URL,
-		data: {'q': asWordObj.word, 'as': 'true'},
-		success: function(data) {
-			
-			console.log(data);
-			if(data.length > 0)
-			{
-				if(data[0] != asWordObj.word)
-				{
-					cleanPartialWord = asWordObj.partial_word.replace(/\W+/g,'');
-					if(data[0].indexOf(cleanPartialWord) != -1)
-					{
-						$asObj.html('&nbsp;'+cleanPartialWord+'<b>'+data[0].replace(cleanPartialWord,'')+'</b>'+'&nbsp;')
-					}
-					else
-					{
-						$asObj.html('&nbsp;'+data[0]+'&nbsp;')
-					}
-					$asObj.show();
-				}
-			}
-		}
-	})
 }
 
 function resetResults(msg) {
@@ -166,8 +56,6 @@ function sendSearchRequest() {
 	var queryText = $("#searchText").val(),
 		searchResults = [];
 		
-	$('#autoSuggestDiv').hide();
-	
 	if($.trim(queryText) == '')
 		return;
 
